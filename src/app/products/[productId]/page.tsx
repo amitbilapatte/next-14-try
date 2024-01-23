8ae44566b6fa80a9dd4dcf60af4ce6445e46046a
Amit Bilapatte <agbilapatte78@gmail.com>
2024-01-23 11:20:04 +0530

    Initial commit

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page({ param }) {
+  return (
+    <div>
+      <h1> `${param.productId}`</h1>
+    </div>
+  );
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Product list:</h1>
+      <h2>1</h2>
+      <h2>2</h2>
+      <h2>3</h2>
+    </div>
+  );
+}
